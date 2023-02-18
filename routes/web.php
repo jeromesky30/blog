e8b3a2be9da1cdbc5bd51122dbedc933d046aee0
jeromesky30 <jtm7124@gmail.com>
2023-02-18 20:58:29 +0800

    i make a web routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/posts', [HomeController::class, 'index'])->name('posts');
+
+Route::get('/create', [PostController::class, 'create'])->name('create');
+//not an blade file, not a form, but rather an action of actual saving of data from the form to the database
+Route::post('store', [PostController::class, 'store'])->name('store');
+Route::get('displaypost/{id}', [PostController::class, 'displaypost'])->name('displaypost');
+Route::get('edit/{id}', [PostController::class, 'edit'])->name('edit');
+Route::post('update', [PostController::class, 'update'])->name('update');
+Route::post('delete', [PostController::class, 'delete'])->name('delete');
+Route::post('destroy/{id}', [PostController::class, 'destroy'])->name('destroy');

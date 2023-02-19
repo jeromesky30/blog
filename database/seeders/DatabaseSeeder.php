f9dc5e551a4ee219ec6640b772d99e637d34541c
jeromesky30 <jtm7124@gmail.com>
2023-02-19 13:34:38 +0800

    added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::create([
+            'name' => 'Jerome Pingol',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin@gmail.com'),
+            'usertype' => 'admin',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::create([
+            'name' => 'Juan Dela Cruz',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('user@gmail.com'),
+            'usertype' => 'user',
+        ]);
     }
 }

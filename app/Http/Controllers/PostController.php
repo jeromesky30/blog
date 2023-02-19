53882acf098ccaf0b856bbb9bda34c3c1ab77d61
jeromesky30 <jtm7124@gmail.com>
2023-02-19 11:33:21 +0800

    make an edit page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -36,4 +36,35 @@ class PostController extends Controller
     return view('displaypost', ['post' => $post]);
 }
 
+public function edit($id)
+{
+    $post = Post::find($id);
+    return view('edit', compact('post'));
+}
+
+
+public function update(Request $request, $id)
+{
+    $validator = Validator::make($request->all(), [
+        'title' => 'required',
+        'content' => 'required',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect()
+            ->route('posts.edit', $id)
+            ->withErrors($validator)
+            ->withInput();
+    }
+
+    $post = Post::find($id);
+    $post->title = $request->title;
+    $post->content = $request->content;
+    $post->save();
+
+    return redirect()->route('posts.show', $id)
+        ->with('success', 'Post updated successfully');
+}
+
+
 }
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,81 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <h1>Edit Post</h1>
 
-<div class="container">
-  <div class="row justify-content-center">
-    <!-- Admin -->
-    <div class="col-md-3">
-      <div class="card">
-        <div class="card-header">{{ __('Hi,') }} {{ Auth::user()->name }}</div>
-      </div>
-      <div class="d-flex flex-column">
-        @if(Auth::user()->usertype == 'admin')
-        <a href="{{ route('home') }}" class="btn btn-primary btn-sm mt-2">All Blogs</a>
-        <a href="{{ route('create') }}" class="btn btn-primary btn-sm mt-2">+ New Blog</a>
-        @endif
-      </div>
+        <form method='POST' action={{ url('posts/update', [$post->id]) }}>
+
+
+            @csrf
+            @method('PUT')
+
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" class="form-control" id="title" name="title" value="{{ $post->title }}" required>
+            </div>
+
+            <div class="form-group">
+                <label for="content">Content</label>
+                <textarea class="form-control" id="content" name="content" rows="10" required>{{ $post->content }}</textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Save Changes</button>
+        </form>
     </div>
-
-    <!-- All Blog Posts -->
-    <div class="col-md-9 bg-light">
-      <div class="fw-bold fs-3">My Daily Blog</div>
-      <!-- Success and Error Alerts -->
-      @if( session('success') )
-      <div class="container px-4">
-        <div class="alert alert-success my-3" role="alert">
-          {{ session('success') }}
-        </div>
-      </div>
-      @endif
-
-      @if( session('error') )
-      <div class="container px-4">
-        <div class="alert alert-danger my-3" role="alert">
-          {{ session('error') }}
-        </div>
-      </div>
-      @endif
-
-      <!-- Create New Blog Form -->
-      <form method="POST" action="{{ route('posts.update', ['post' => $post->id]) }}">
-        @csrf
-        @method('PUT')
-        <div class="mb-3">
-            <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $post->title) }}" required>
-        </div>
-        <div class="mb-3">
-            <label for="content" class="form-label">Content</label>
-            <textarea class="form-control" id="content" name="content" rows="10" required>{{ old('content', $post->content) }}</textarea>
-        </div>
-        <div class="mb-3">
-            <button type="submit" class="btn btn-primary">Update Post</button>
-        </div>
-    </form>
-
-      <!-- All Blog Posts -->
-      @foreach($posts as $post)
-      <div class="card my-3">
-        <div class="card-header fw-bold">
-          {{ $post->title }}
-          @if(Auth::user()->usertype == 'admin')
-          <div class="float-end">
-            <a href="{{ route('edit', $post->id) }}" class="btn btn-primary btn-sm">Edit</a>
-            <a href="{{ route('delete', $post->id) }}" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this post?')">Delete</a>
-          </div>
-          @endif
-        </div>
-        <div class="card-body">
-          {{ $post->content }}
-        </div>
-      </div>
-      @endforeach
-
-    </div>
-
-  </div>
-</div>
-
-
 @endsection
